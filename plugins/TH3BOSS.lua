e7ff596ce9e8355982b4186f8fa553500d35f152
ds4ww <30879462+ds4ww@users.noreply.github.com>
2017-08-10 04:32:48 +0300

    Update TH3BOSS.lua

--- plugins/TH3BOSS.lua
+++ plugins/TH3BOSS.lua
@@ -6,7 +6,7 @@ if ( msg.text ) then
 
   if ( msg.to.type == "user" ) then
 
-     return "للتحدث مع المطور اضغط على المعرف التالي \n ▫️ @TH3BOSS \n او اذا محظور اضغط هنا \n ▫️  @ll60Kllbot\n  قناة الـسـورس \n ▫️  @llDEV1ll\n مـطـور الـسـورس\n الــــزعـــيـــــم > @TH3BOSS ▫️ "
+     return "للتحدث مع المطور اضغط على المعرف التالي▫️ @ds4ww \n "
   end
    
 end 
